1fab99fae03005829cb048927ac58681ccb6b12f
Mateusz Parzys <mateuszparzys123@gmail.com>
2026-06-19 10:24:43 +0200

    init commit

---
+++ src/main/java/com/example/ticketer/TicketerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticketer/TicketerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
